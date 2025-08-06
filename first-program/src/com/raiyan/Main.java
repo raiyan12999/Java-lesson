afe20525ae904a543edc44ed2054ae72d7198cf1
raiyan12999 <raiyanbinatik.m29@gmail.com>
2025-08-06 03:27:35 -0700

    Initialized everything

---
+++ first-program/src/com/raiyan/Main.java
@@ -0,0 +1,11 @@
+package com.raiyan;
+
+public class Main{
+    public static void main(String[] args){
+        // If you want a new line use println, if you don't use print
+
+        System.out.println("Hello World!");
+        System.out.print("I am checking the function of new line ");
+        System.out.println("This one is with new line");
+    }
+}
